33b725808ff36c27d921c7846b920db1837d2219
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-01-13 08:22:36 +0200

    Added initial files

--- README.md
+++ README.md
@@ -2,3 +2,66 @@ jquery-nor-rest
 ===============
 
 jQuery REST client plugin for Sendanor REST APIs
+
+
+Usage
+-----
+
+First make sure you include our jQuery plugin file `jquery.nor.rest.js`.
+
+### Fetching data with GET requests
+
+When `/api` returns:
+
+```javascript
+{
+  "$ref": "http://zeta3-lts-local:3000/api",
+  "profile": {
+    "$ref": "http://zeta3-lts-local:3000/api/profile"
+  }
+}
+```
+
+...and `/api/profile` returns:
+
+```javascript
+{
+  "$ref": "http://zeta3-lts-local:3000/api/profile",
+  "username": "foo",
+  "email": "foo@example.com"
+}
+```
+
+Then you can call `$.nor.rest.get(url[, params])` like this:
+
+```javascript
+$.nor.rest.get('/api').done(function(api) {
+	if(api.profile) {
+
+		// Get full user resource. Calling `api.profile()` is same as calling 
+		// `$.nor.rest.get('/api/profile')`.
+		api.profile().done(function(profile) {
+			console.log( profile );
+		});
+
+	}
+});
+```
+
+### Changing the data with POST request
+
+In this example the POST handler redirects the user back to `/api/profile`:
+
+```javascript
+$.nor.rest.get('/api').done(function(api) {
+	if(api.profile) {
+
+		// Get full user resource. Calling `api.profile.post()` is same as calling 
+		// `$.nor.rest.post('/api/profile')`.
+		api.profile.post({'foo':'bar'}).done(function(profile) {
+			console.log( profile );
+		});
+
+	}
+});
+```
---
+++ src/jquery.nor.rest.js
@@ -0,0 +1,178 @@
+/** HTTP REST client plugin for jQuery */
+(function ( $ ) {
+	if( $.nor === undefined ) { $.nor = {}; }
+
+	/** Copy object */
+	function copy(o) {
+		return JSON.parse(JSON.stringify(o));
+	}
+
+	/** 
+	 * Returns a copy of variable `from` with any child objects with a property `$ref` 
+	 * recursively converted as `function(opts)` which will call `Resource.get($ref, opts)`.
+	 */
+	function ref_copy(from) {
+		//console.log( 'at ref_copy(): from = ' + JSON.stringify(from) );
+		//console.log( 'typeof from = ' + (typeof from) );
+
+		var to;
+
+		// Handle arrays
+		if( from && (typeof from === 'object') && (from instanceof Array) ) {
+			//console.log( '^ from is Array: ' + JSON.stringify(from) );
+
+			throw new TypeError("Array support unimplemented.");
+
+			to = from.map(ref_copy);
+
+		// Convert objects with a property `$ref` as a function shortcut to `Resource.get(url, opts)`;
+		} else if( from && (typeof from === 'object') && (from['$ref'] !== undefined) ) {
+			//console.log( '^ from is $ref-object: ' + JSON.stringify(from) );
+
+			var url = from['$ref'];
+			to = function(opts) {
+				return Resource.get(url, opts);
+			};
+
+			to.post = function(opts) {
+				return Resource.post(url, opts);
+			};
+			to.update = to.post;
+
+			to.del = function(opts) {
+				return Resource.del(url, opts);
+			};
+
+			ref_copy_self(to, from);
+
+		// Convert normal objects
+		} else if( from && (typeof from === 'object') ) {
+
+			to = {};
+			//console.log( '^ from is plain object: ' + JSON.stringify(from) );
+			ref_copy_self(to, from);
+
+		// Anything else is passed directly back
+		} else {
+			//console.log( '^ from is other type: ' + JSON.stringify(from) );
+			to = from;
+		}
+
+		//console.log('..returning ' + JSON.stringify(to) + ' of type ' + (typeof to));
+		return to;
+	}
+
+	/** Copies recursively any child objects with property a `$ref` as a function calling `Resource.get()`
+	 * @param self {object|function} The object where the properties should be copied
+	 * @param obj {object} The object from the properties should be copied
+	 */
+	function ref_copy_self(self, obj) {
+		//console.log( 'at ref_copy_self(): self = ' + JSON.stringify(self) + ', obj = ' + JSON.stringify(obj) );
+		Object.keys(obj).forEach(function(key) {
+			self[key] = ref_copy(obj[key]);
+		});
+	}
+
+	/** Resource constructor
+	 * @param obj {object} The resource object
+	 */
+	function Resource(obj) {
+		//console.log( 'at new Resource(): obj = ' + JSON.stringify(obj) );
+		var self = this;
+		obj = copy(obj);
+		ref_copy_self(self, obj);
+	}
+
+	/** Get JSON REST resource at `url` and returns promise of an interface */
+	Resource.get = function(url, params) {
+		//console.log(' at Resource.get(' + JSON.stringify(url) + ')' );
+
+		var jqxhr = $.ajax({
+		  dataType: "json",
+		  url: url,
+		  data: params,
+		  cache: false
+		});
+
+		var res = jqxhr.then(function success_handler(data) {
+			//console.log( 'at jqxhr.then(): data = ' + JSON.stringify( data ) );
+			if(data.$ref === undefined) {
+				//console.log('Warning! Resource from ' + url + ' did not have $ref property. Using ' + url + ' instead.' );
+				data.$ref = url;
+			}
+
+			var res = new Resource(data);
+			//console.log('res.session is type of ' + typeof res.session);
+			return res;
+		}, function fail_handler(err) {
+			//console.log( 'at jqxhr.fail(): err = ' + JSON.stringify( err ) );
+			throw err;
+		});
+		return res;
+	};
+
+	/** POST to the JSON REST resource at `url`. Returns a promise of an interface. */
+	Resource.post = function(url, params) {
+		//console.log(' at Resource.get(' + JSON.stringify(url) + ')' );
+
+		var jqxhr = $.ajax({
+		  type: 'POST',
+		  dataType: "json",
+		  url: url,
+		  data: params
+		});
+
+		var res = jqxhr.then(function success_handler(data) {
+			//console.log( 'at jqxhr.then(): data = ' + JSON.stringify( data ) );
+			if(data.$ref === undefined) {
+				//console.log('Warning! Resource from ' + url + ' did not have $ref property. Using ' + url + ' instead.' );
+				data.$ref = url;
+			}
+
+			var res = new Resource(data);
+			//console.log('res.session is type of ' + typeof res.session);
+			return res;
+		}, function fail_handler(err) {
+			//console.log( 'at jqxhr.fail(): err = ' + JSON.stringify( err ) );
+			throw err;
+		});
+		return res;
+	};
+
+	/** DELETE to the JSON REST resource at `url`. Returns a promise of an interface. */
+	Resource.del = function(url, params) {
+		//console.log(' at Resource.get(' + JSON.stringify(url) + ')' );
+
+		params = params || {};
+		params._method = 'DELETE';
+
+		var jqxhr = $.ajax({
+		  type: 'POST',
+		  dataType: "json",
+		  url: url,
+		  data: params
+		});
+
+		var res = jqxhr.then(function success_handler(data) {
+			//console.log( 'at jqxhr.then(): data = ' + JSON.stringify( data ) );
+			if(data.$ref === undefined) {
+				//console.log('Warning! Resource from ' + url + ' did not have $ref property. Using ' + url + ' instead.' );
+				data.$ref = url;
+			}
+
+			var res = new Resource(data);
+			//console.log('res.session is type of ' + typeof res.session);
+			return res;
+		}, function fail_handler(err) {
+			//console.log( 'at jqxhr.fail(): err = ' + JSON.stringify( err ) );
+			throw err;
+		});
+		return res;
+	};
+
+	// Export as `$.nor.rest`
+	$.nor.rest = Resource;
+
+}( jQuery ));
+
+/* EOF */
